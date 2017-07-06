b2eea8394fabe9d4d0a6fbbcd1965ffed4a0bd78
“Harith <hneralla56@gmail.com>
2017-07-06 11:09:50 -0700

    changed formatting

--- bash_profile.sh
+++ bash_profile.sh
@@ -19,24 +19,24 @@ alias cp="cp -i" # prompt user when overwriting
 alias mv="mv -i" # prompt user when moving 
 
 # Miscellaneous 
-alias df="df -h"					          # Always print in human readable form
+alias df="df -h"                    # Always print in human readable form
 alias cpwd='pwd|tr -d "\n"|pbcopy'  # Copy the working directory path
-alias f='open -a Finder ./'			    # Opens current directory in MacOS Finder
-alias edit='subl'					          # Opens Sublime Text	
-alias c="clear"						          # Clear terminal display					
-alias cls="clear"					          # Clear terminal display
-alias h="history"					          # Show terminal history
-alias ~="cd ~"						          # Go home
-alias ..="cd .."					          # Go to parent directory
-alias ...='cd ../../'				        # Go back 2 directory levels
-alias .3='cd ../../../'				      # Go back 3 directory levels
-alias .4='cd ../../../../'			    # Go back 4 directory levels
-alias .5='cd ../../../../../'		    # Go back 5 directory levels
-alias .6='cd ../../../../../../'	  # Go back 6 directory levels
-alias cdr="cd repos"				        # Go to repositories
-alias qfind="find . -name "			    # Quickly search for file
+alias f="open -a Finder ./"         # Opens current directory in MacOS Finder
+alias edit="subl"                   # Opens Sublime Text	
+alias c="clear"                     # Clear terminal display					
+alias cls="clear"                   # Clear terminal display
+alias h="history"                   # Show terminal history
+alias ~="cd ~"	                     # Go home
+alias ..="cd .."                    # Go to parent directory
+alias ...="cd ../../"               # Go back 2 directory levels
+alias .3="cd ../../../"             # Go back 3 directory levels
+alias .4="cd ../../../../"          # Go back 4 directory levels
+alias .5="cd ../../../../../"       # Go back 5 directory levels
+alias .6="cd ../../../../../../"    # Go back 6 directory levels
+alias cdr="cd repos"                # Go to repositories
+alias qfind="find . -name "         # Quickly search for file
 
 alias gpm="git push origin master"
 
-zipf () { zip -r "$1".zip "$1" ; }  							            # To create a ZIP archive of a folder
-q () { osascript -e 'tell application "Terminal" to quit' ;}	# To exit and quit Terminal 
+zipf () { zip -r "$1".zip "$1" ; }                              # To create a ZIP archive of a folder
+q () { osascript -e 'tell application "Terminal" to quit' ; }   # To exit and quit Terminal 
